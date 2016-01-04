de17f992fd8749c73ae6b7d08f2ffbb82914891a
Grady O'Connell <flipcoder@gmail.com>
2016-01-03 20:02:14 -0800

    get jealous of other bot's usage

---
+++ plugins/jealous.py
@@ -0,0 +1,14 @@
+import time
+throttle = 5.0
+last_t = 0
+
+def jealous(ctx, serv, nick, dest, msg):
+    global last_t
+    if msg.startswith("!") and (msg+"a")[1:].isalpha():
+        t = time.time()
+        if t - last_t > throttle:
+            last_t = t
+            serv.say(dest, ":'(")
+        
+serv.on_msg.connect(jealous)
+
