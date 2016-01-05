ba01565e69dee24381e0a0843d10d4a0074a09f4
Grady O'Connell <flipcoder@gmail.com>
2016-01-05 06:31:55 -0800

    diff matching

--- plugins/jealous.py
+++ plugins/jealous.py
@@ -4,7 +4,7 @@ last_t = 0
 
 def jealous(ctx, serv, nick, dest, msg):
     global last_t
-    if msg.startswith("!") and msg[1:].isalnum():
+    if msg.startswith("!") and msg[1] != "!"
         t = time.time()
         if t - last_t > throttle:
             last_t = t
